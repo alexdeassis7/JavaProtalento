10e03ab2e740b16915926fcea547f1cb2cc15ad2
User <User@192.168.0.114>
2021-11-16 10:19:50 -0500

    mayor f2 programa mayor de tres

---
+++ pruebas/src/main/java/clase3/Mayorf2.java
@@ -0,0 +1,63 @@
+package clase3;
+//import java.util.Scanner;
+public class Mayorf2 {
+
+	public static void main(String[] args) {
+		/*Scanner teclado=new Scanner(System.in);
+		//determinar dado tres nuneros cual es el mayor
+		//necesito 3
+		//if
+		//mostrar
+		int n1,n2,n3;
+		System.out.println("ingrese  el primer numero");
+		n1=teclado.nextInt();
+		
+		System.out.println("ingrese  el segundo numero");
+		n2=teclado.nextInt();
+		
+		System.out.println("ingrese  el terecr numero");
+		n3=teclado.nextInt();
+		if(n1>n2 && n1>n3) {
+			System.out.println(n1);
+		}if(n2>n3 && n2>n1) {
+			System.out.println(n2);
+		}if(n3>n1 && n3>n2) {
+			System.out.println(n3);
+		}
+		if(n1==n2 && n2==n3) {
+			System.out.println("iguales");
+		}
+		//n1 y n2<n3
+		//n1 y n3>n2
+		//n3 y n2<n1
+		if(n1==n2 && n2<n3) {
+			System.out.println(n3);
+			
+		}else if(n1==n3 && n2>n3) {
+			System.out.println(n2);
+		}else if(n3==n2 && n2<n1) {
+			System.out.println(n1);
+		}else if(n1==n2 && n1>n3) {
+			System.out.println(n1);
+			
+		}else if(n2==n3 && n3>n1) {
+			System.out.println(n2);
+			
+		}else if(n1==n3 && n3>n2) {
+			System.out.println(n1);
+			
+		}*/
+		int n1=11,n2=12,n3=12;
+		int may=n1;
+		if(may<=n2) {
+		may=n2;
+			
+		}if(may<=n3) {
+			may=n2;
+		}System.out.println("el mayor de los nuemros es"+ may);
+		
+		
+
+	}
+
+}
