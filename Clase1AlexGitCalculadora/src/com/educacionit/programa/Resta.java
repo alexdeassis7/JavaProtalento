46e4749d1954e3a35f0609d8ac75e8285ff1172a
johaoduran27 <johaoduranse@gmail.com>
2022-02-01 20:27:11 -0500

    se crea funcion resta

---
+++ Clase1AlexGitCalculadora/src/com/educacionit/programa/Resta.java
@@ -0,0 +1,15 @@
+package com.educacionit.programa;
+
+public class Resta {
+
+	public static void main(String[] args) {
+		int a= 10;
+		int b=5;
+		
+		int resul=a-b;
+		
+		System.out.println(resul);
+
+	}
+
+}
