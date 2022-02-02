26d2f69ace7a7947663e1f66201df3afb383c0df
User <User@192.168.0.103>
2022-02-01 21:05:16 -0500

    funcion x al cuadrado

---
+++ shoppingcart/src/main/java/primerClaseAlex/FuncionXalcuadrado.java
@@ -0,0 +1,16 @@
+package primerClaseAlex;
+
+public class FuncionXalcuadrado {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("mi funcion x**2 es una parabola que varia de cudrante dentro del plano cartesiano segun el signo o si es y**2");
+		
+		
+		
+		
+
+	}
+
+}
