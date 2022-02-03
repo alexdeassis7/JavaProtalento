f194ceffb586b4129c3bb071ef13fbf9d30841b1
Hernan Chapid <hernan0740@gmail.com>
2022-02-03 18:03:20 -0500

    potencia

--- Clase1AlexGitCalculadora/src/com/educacionit/programa/Calculadora.java
+++ Clase1AlexGitCalculadora/src/com/educacionit/programa/Calculadora.java
@@ -2,4 +2,12 @@ package com.educacionit.programa;
 
 public class Calculadora {
 
+	
+	
+	public int suma(int a, int b) {
+		
+	int	suma=a+b;
+		
+		return suma;
+	}
 }
---
+++ Clase1AlexGitCalculadora/src/com/educacionit/programa/Potencia.java
@@ -0,0 +1,12 @@
+package com.educacionit.programa;
+
+public class Potencia {
+
+	public int obtenerPotencia(int a, int b) {
+		
+		int	potencia=a^b;
+			
+			return potencia;
+		}
+	
+}
