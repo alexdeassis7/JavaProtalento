1de51a577134b81a82c02d31216543a89d754852
Juan David Saavedra <juansaavedra2406@gmail.com>
2022-02-03 18:34:10 -0500

    Juan Saavedra Raiz

--- Clase1AlexGitCalculadora/src/com/educacionit/programa/Raiz.java
+++ Clase1AlexGitCalculadora/src/com/educacionit/programa/Raiz.java
@@ -1,5 +1,15 @@
 package com.educacionit.programa;
 
 public class Raiz {
-
+	float indice;
+	float radical;
+	float raiz;
+	public float CalculaRaiz(float indice,float radical) {
+		
+		raiz=(float) Math.pow(radical, indice);
+		
+		
+		return raiz;
+		
+	}
 }
