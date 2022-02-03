57b9e350b69aed2b3160d8c8d41fc856c4f79dd7
jacevehe <jeacher_8@hotmail.com>
2022-02-03 18:33:58 -0500

    Task 1 OK

---
+++ Clase1AlexGitCalculadora/src/com/educacionit/programa/Cociente.java
@@ -0,0 +1,15 @@
+package com.educacionit.programa;
+
+public class Cociente {
+	
+	
+	int numero1; 
+	int numero2;
+	int res;
+	
+	public int cociente() {
+		this.res=this.numero1 % this.numero2;
+		return res;
+	}
+
+}
