29526291db2d4cfdca9a37cc781c0d64ea6ad3e0
Alex <alexdeassis7@gmail.com>
2022-02-03 22:48:53 -0300

    conexión a base de datos

---
+++ clase2RepasoDB/src/com/educacionit/database/Conexion.java
@@ -0,0 +1,41 @@
+package com.educacionit.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+	static String db = "nombreDeMiBaseDeDatos";
+	static String login = "root";
+	static String password = "";
+	static String url = "jdbc:mysql://localhost/" + db;
+
+	Connection conn = null;
+
+	public Conexion() {
+		try {
+			// obtenemos el driver para mysql
+			 Class.forName("com.mysql.jdbc.Driver");
+
+			// obtenemos la conexion
+			conn = DriverManager.getConnection(url, login, password);
+			if (conn != null) {
+				System.out.println("Se conecto a la base de dato " + db);
+			}
+		} catch (SQLException e) {
+			System.out.println("SQLException");
+			e.printStackTrace();
+		} catch (Exception e) {
+
+			System.out.println("Exception");
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Conexion miConexion = new Conexion();
+	}
+
+}
