46b2b447605bdb892770e1d344ab29017c738b41
Alex <alexdeassis7@gmail.com>
2022-03-12 18:09:22 -0300

    Clase 2 Repaso Maven Protalento

---
+++ tutorial_maven/turorial_maven_jar/src/main/java/com/educacionit/App.java
@@ -0,0 +1,13 @@
+package com.educacionit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
